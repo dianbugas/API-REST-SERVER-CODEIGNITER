b1ab16c10dfe6ea2c0a764a75428bd8736169a06
dianynf <dianbugas@gmail.com>
2019-02-12 10:24:55 +0700

    inde_get

--- application/controllers/api/Mahasiswa.php
+++ application/controllers/api/Mahasiswa.php
@@ -38,4 +38,54 @@ class Mahasiswa extends REST_Controller
             ], REST_Controller::HTTP_NOT_FOUND);
         }
     }
+
+    public function index_delete()
+    {
+        $id = $this->delete('id');
+
+        if ($id === null) {
+            $this->response([
+                'status' => false,
+                'message' => 'provide an id'
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        } else {
+            if ($this->mahasiswa->deleteMahasiswa($id) > 0) {
+                //ok jalan
+                $this->response([
+                    'status' => true,
+                    'id' => $id,
+                    'message' => 'deleted.'
+                ], REST_Controller::HTTP_NO_CONTENT);
+            } else {
+                //not found
+                $this->response([
+                    'status' => false,
+                    'message' => 'id not found'
+                ], REST_Controller::HTTP_BAD_REQUEST);
+            }
+        }
+    }
+
+    public function index_post()
+    {
+        $data = [
+            'nrp' => $this->post('nrp'),
+            'nama' => $this->post('nama'),
+            'email' => $this->post('email'),
+            'jurusan' => $this->post('jurusan')
+        ];
+
+        if ($this->mahasiswa->createMahasiswa($data) > 0) {
+            $this->response([
+                'status' => true,
+                'message' => 'new mahasiswa has been created'
+            ], REST_Controller::HTTP_CREATED);
+        } else {
+            //not found
+            $this->response([
+                'status' => false,
+                'message' => 'falied to create new data!'
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
 }
--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -2,8 +2,24 @@
 
 class Mahasiswa_model extends CI_Model
 {
-    public function getMahasiswa()
+    public function getMahasiswa($id = null)
     {
-        return $this->db->get('mahasiswa')->result_array();
+        if ($id === null) {
+            return $this->db->get('mahasiswa')->result_array();
+        } else {
+            return $this->db->get_where('mahasiswa', ['id' => $id])->result_array();
+        }
+    }
+
+    public function deleteMahasiswa($id)
+    {
+        $this->db->delete('mahasiswa', ['id' => $id]);
+        return $this->db->affected_rows();
+    }
+
+    public function createMahasiswa($data)
+    {
+        $this->db->insert('mahasiswa', $data);
+        return $this->db->affected_rows();
     }
 }
